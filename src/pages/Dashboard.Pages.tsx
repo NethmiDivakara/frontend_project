e7894c8cc9477d397a4958f408e0916cfd5d0bc7
Nethmi Divakara <nethmidivakara@gmail.com>
2026-08-26 14:28:50 +0530

    feat(ui):add Dashboard component with a button

---
+++ src/pages/Dashboard.Pages.tsx
@@ -0,0 +1,8 @@
+import {Button} from '@/components/ui/button'
+
+
+<div className="flex flex-col items-center justify-center h-screen">
+    <Button variant = "destructive" className="mb-4">
+        Destructive Button
+        </Button>
+        </div>
